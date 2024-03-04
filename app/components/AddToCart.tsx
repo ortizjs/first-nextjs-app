27473fa04746762e21e6d0e82d24835e886263b0
Jon Ortiz <jonnatano94@gmail.com>
2024-03-04 12:53:39 -0800

    Add initial components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button className='btn btn-primary'>Add to Cart</button>
+    </div>
+  )
+}
+
+export default AddToCart;
